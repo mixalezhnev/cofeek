13e5afa5b960a03fa63acf563be414c747846165
Alexander Karelin <pyxarez@gmail.com>
2017-04-26 18:12:58 +0400

    async action cart

--- src/store/actions/CartActions.js
+++ src/store/actions/CartActions.js
@@ -7,28 +7,31 @@ import { database } from '../../firebaseApp.js';
 
 const cartProductsRef = database.ref('cart'); 
 
+const gettingCartItemsSuccess = (snapshot) => ({
+  type: CART_RECEIVE_DATA,
+  payload: {
+    data: snapshot.val()
+  }
+});
+
+const gettingCartItemsFailure = (error) => ({
+  type: CART_RECEIVE_DATA_ERROR,
+  payload: {
+    message: error.message
+  }
+});
+
 export const toggleCartState = () => ({
     type: TOGGLE_CART_STATE
 });
 
 export const listenToCart = () => {
-  return (dispatch) => {
-    return cartProductsRef.once('value')
-      .then(snapshot => {
-        dispatch({
-          type: CART_RECEIVE_DATA,
-          payload: {
-            data: snapshot.val()
-          }
-        });
-      })
-      .catch(error => {
-        dispatch({
-          type: CART_RECEIVE_DATA_ERROR,
-          payload: {
-            message: error.message
-          }
-        });
-      });
+  return async (dispatch) => {
+    try {
+      const snapshot = await cartProductsRef.once('value');
+      dispatch(gettingCartItemsSuccess(snapshot));
+    } catch(error) {
+      dispatch(gettingCartItemsFailure(error));
+    }
   };
 };
